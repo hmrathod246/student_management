256c7327b67d252afd044874a338ff535f1459cb
hansa rathod <hansarathod113@gmail.com>
2026-02-02 13:31:51 +0530

    [HM][default router added in app.jsx file]

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import { Header } from "./components/Header";
 import "./App.css";
 import { Login } from "./pages/Login";
 import Register from "./pages/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+const DefaultRoute=()=>{
+  const authData=JSON.parse(localStorage.getItem('authData'));
+if(authData)
+{
+  return<Navigate to="/login" replace/>
+}
+return<Navigate to="/register" replace/>
+}
 function App() {
   const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />,
+    },
     {
       path: "/login",
       element: <Login />,
